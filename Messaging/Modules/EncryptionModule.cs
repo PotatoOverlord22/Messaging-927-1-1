da338ff518fd772742e97576aa5e2f6831df50c4
Dragos <talaba.dragos@gmail.com>
2024-03-21 16:35:56 +0200

    modified EncryptionModule.cs

--- Messaging/Modules/EncryptionModule.cs
+++ Messaging/Modules/EncryptionModule.cs
@@ -6,7 +6,65 @@ using System.Threading.Tasks;
 
 namespace Messaging.Modules
 {
-    internal class EncryptionModule
+    public class EncryptionModule
     {
+        private string plainText;
+
+        private string key;
+
+        public EncryptionModule(string text)
+        {
+            GenerateKey();
+            plainText = text;
+        }
+
+        public string getPlainText()
+        {
+            return plainText;
+        }
+
+        public void setPlainText(string text)
+        {
+            plainText = text;
+        }
+
+        public string getKey()
+        {
+            return key;
+        }
+
+        public void setKey(string newKey)
+        {
+            this.key = newKey;
+        }
+
+        private void GenerateKey()
+        {
+
+            int keyLength = 16;
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < keyLength; i++)
+            {
+                sb.Append((char)random.Next(32, 127));
+            }
+            key = sb.ToString();
+        }
+
+        public string Encrypt()
+        {
+            StringBuilder encryptedText = new StringBuilder();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                encryptedText.Append((char)(plainText[i] ^ this.key[i % this.key.Length]));
+            }
+            plainText = encryptedText.ToString();
+            return plainText;
+        }
+
+        public string Decrypt()
+        {
+            return Encrypt();
+        }
     }
 }
