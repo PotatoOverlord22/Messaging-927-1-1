6d2bc9ef26269a7824d5abb45b2b0e453dbeb6ce
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-03-21 15:34:14 +0200

    app structure

---
+++ Messaging/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Please don't modify Main for now :)
+            Console.WriteLine("Starting Messaging app...");
+        }
+    }
+}
