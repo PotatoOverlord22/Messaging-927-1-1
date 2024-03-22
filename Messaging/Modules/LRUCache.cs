43d4fc43f9bd6d6060759cc35106f6872d091ab4
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-03-22 02:41:20 +0200

    added LRU cache with tests

---
+++ Messaging/Modules/LRUCache.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.Modules
+{
+    public class LRUCache<TKey, TValue>
+    {
+        /* Implementation of a LRU cache with a set capacity
+         *  TKey and TValue are generics used for more flexibility of the module
+         */
+        // Max capacity of our cache
+        private int _capacity { get; }
+        private Dictionary<TKey, TValue> _cacheMap;
+        // We need a data type with O(1) beggining and end operations, wanted to use a Dequeue (there isn't one in c#), so a DLL would suffice.
+        // The list is used to decide which item needs to be kicked out of the cache (least recently used rule)
+        // choosing to use (TKey, TValue) to uniquely identify each value
+        private LinkedList<(TKey, TValue)> _cacheQueue;
+
+        // The above shoudl be read only, that's why I'm making only getters
+        public int Capacity { get { return _capacity; } }
+        public Dictionary<TKey, TValue> CacheMap { get { return _cacheMap; } }
+        public LinkedList<(TKey, TValue)> CacheQueue { get { return _cacheQueue; } }
+
+
+        public LRUCache(int capacity)
+        {
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException("Cache capacity should be bigger than 0, provided capacity: " + capacity);
+            this._capacity = capacity;
+            _cacheMap = new Dictionary<TKey, TValue>();
+            _cacheQueue = new LinkedList<(TKey, TValue)>();
+        }
+
+        public bool IsKeyInCache(TKey key)
+        {
+            return _cacheMap.ContainsKey(key);
+        }
+
+        public bool IsValueInCache(TValue value)
+        {
+            return _cacheMap.ContainsValue(value);
+        }
+
+        public bool TryToGetValue(TKey key, out TValue value)
+        {
+            // Return default TValue if the key is not in the map
+            if (!_cacheMap.ContainsKey(key)){
+                value = default(TValue);
+                return false;
+            }
+
+            // This means we have the value with the given key in our cache
+            value = _cacheMap[key];
+            // Put the value in front of the queue
+            _cacheQueue.Remove((key, value));
+            _cacheQueue.AddFirst((key, value));
+            return true;
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            // Check if it's already in the map
+            if (_cacheMap.ContainsKey(key))
+            {
+                // Just update it as recently used if it is in the map (aka add it in front of the queue)
+                _cacheMap[key] = value;
+                _cacheQueue.Remove((key, value));
+                _cacheQueue.AddFirst((key, value));
+                return;
+            }
+            // Since we didn't find it in the cache we have to actually add it
+            // Check if we have space or we must kick an item out
+            if (_cacheMap.Count >= _capacity)
+            {
+                // Kick the last item both from the queue and from the map
+                var lastItem = _cacheQueue.Last();
+                _cacheQueue.RemoveLast();
+                _cacheMap.Remove(lastItem.Item1);
+            }
+
+            // Add them to the map and to the queue
+            _cacheMap[key] = value;
+            _cacheQueue.AddFirst((key, value));
+        }
+
+        public void Clear()
+        {
+            _cacheMap.Clear();
+            _cacheQueue.Clear();
+        }
+    }
+}
---
+++ UnitTests/ModuleTests/LRUCacheTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messaging.Modules;
+
+namespace UnitTests.ModuleTests
+{
+    [TestFixture]
+    public class LRUCacheTests
+    {
+        private LRUCache<int, string> cache;
+        [Test]
+        public void TestInit()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { new LRUCache<int, int>(-1); }, "Cache capacity should be bigger than 0, provided capacity: -1");
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.cache = new LRUCache<int, string>(5);
+            for (int i = 0; i < 5; i++)
+            {
+                // this adds 0 -> 'a'
+                //           1 -> 'b'
+                //           2 -> 'c' and so on
+                cache.Put(i, Convert.ToString(Convert.ToChar(Convert.ToInt32('a') + i)));
+            }
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            this.cache.Clear();
+        }
+
+        [Test]
+        public void TestPut() 
+        {
+            int i, j;
+            Assert.IsTrue(cache.IsKeyInCache(0));
+            Assert.IsTrue(cache.IsKeyInCache(1));
+            Assert.IsTrue(cache.IsKeyInCache(2));
+            Assert.IsTrue(cache.IsKeyInCache(3));
+            Assert.IsTrue(cache.IsKeyInCache(4));
+            Assert.IsFalse(cache.IsKeyInCache(5));
+            Assert.IsFalse(cache.IsKeyInCache(6));
+            Assert.IsFalse(cache.IsKeyInCache(10));
+            Assert.IsFalse(cache.IsKeyInCache(int.MaxValue));
+            Assert.IsFalse(cache.IsKeyInCache(-int.MaxValue));
+
+            // Now let's check if the least recently used value is kicked when we try to add a new value
+            cache.Put(10, "new value");
+            Assert.IsTrue(cache.IsKeyInCache(10));
+            Assert.IsFalse(cache.IsKeyInCache(0));
+
+            cache.Put(0, "0 is back");
+            Assert.IsTrue(cache.IsKeyInCache(0));
+            Assert.IsFalse(cache.IsKeyInCache(1));
+
+            // try bigger numbers
+            int size = 100;
+            var bigCache = new LRUCache<int, int>(size);
+            for (i = 1; i <= size; ++i)
+            {
+                bigCache.Put(i, i);
+            }
+            for (i = size + 1; i <= 2*size; ++i)
+            {
+                bigCache.Put(i, i);
+                Assert.IsTrue(bigCache.IsKeyInCache(i));
+                Assert.IsFalse(bigCache.IsKeyInCache(i - size));
+            }
+        }
+        [Test]
+        public void TestTryToGet()
+        {
+            string value;
+            // Check if the values are there or not
+            Assert.IsTrue(cache.TryToGetValue(0, out value));
+            Assert.IsTrue(cache.TryToGetValue(1, out value));
+            Assert.IsTrue(cache.TryToGetValue(2, out value));
+            Assert.IsTrue(cache.TryToGetValue(3, out value));
+            Assert.IsTrue(cache.TryToGetValue(4, out value));
+            Assert.IsFalse(cache.TryToGetValue(5, out value));
+
+            // Check the actual values now
+            cache.TryToGetValue(0, out value);
+            Assert.AreEqual(value, "a");
+            cache.TryToGetValue(1, out value);
+            Assert.AreEqual(value, "b");
+            cache.TryToGetValue(2, out value);
+            Assert.AreEqual(value, "c");
+            cache.TryToGetValue(3, out value);
+            Assert.AreEqual(value, "d");
+            cache.TryToGetValue(4, out value);
+            Assert.AreEqual(value, "e");
+            cache.TryToGetValue(5, out value);
+            Assert.AreNotEqual(value, "f");
+            
+            // try to get a value that is not in the cache and check against the default value
+            // default value for strings is null
+            cache.TryToGetValue(int.MaxValue, out value);
+            Assert.AreEqual(value, null);
+
+            // try bigger numbers
+            int i;
+            int size = 100;
+            int intValue;
+            var bigCache = new LRUCache<int, int>(size);
+            for (i = 1; i <= size; ++i)
+            {
+                bigCache.Put(i, i);
+            }
+            for (i = 1; i <= size; ++i)
+            {
+                for (int j = 1; j <= size; ++j)
+                {
+                    bigCache.TryToGetValue(j, out intValue);
+                    Assert.AreEqual(intValue, j);
+                }
+            }
+        }
+    }
+}
