b9375e4dc444b1d81902993829f8187e25000cc2
cosmintimis <timis.cosmin10@gmail.com>
2024-03-22 10:31:49 +0200

    huffman compression module

---
+++ Messaging/Modules/Data compression module/HuffmanDataCompression.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_compression
+{
+    internal class HuffmanDataCompression
+    {
+
+        string inputFilePath;
+        HuffmanTree huffmanTree;
+        char[]? allCharactersFromInputFile;
+
+        public HuffmanDataCompression(string inputFilePath)
+        {
+            this.inputFilePath = inputFilePath;
+            Dictionary<char, int> characterFrequencyDictionary = ReadInputFile();
+            huffmanTree = new HuffmanTree();
+            huffmanTree.BuildTreeFromDictionary(characterFrequencyDictionary);
+            CreateKeyFile();
+            CompressData();
+        }
+
+
+        public Dictionary<char, int> ReadInputFile()
+        {
+            string inputData = "";
+            if (File.Exists(inputFilePath))
+            {
+                inputData = File.ReadAllText(inputFilePath);
+            }
+
+            this.allCharactersFromInputFile = inputData.ToCharArray();
+            Dictionary<char, int> characterFrequencyDictionary = new Dictionary<char, int>();
+            foreach (char c in allCharactersFromInputFile)
+            {
+                if (characterFrequencyDictionary.ContainsKey(c))
+                {
+                    characterFrequencyDictionary[c] = characterFrequencyDictionary[c] + 1;
+                }
+                else
+                {
+                    characterFrequencyDictionary.Add(c, 1);
+                }
+            }
+
+            return characterFrequencyDictionary;
+        }
+
+
+        public void CreateKeyFile()
+        {
+
+            Dictionary<char, string> characterCodeDictionary = huffmanTree.GetCharacterCodeDictionary();
+
+            string keyFilePath = inputFilePath.Replace("input.txt", "key.txt");
+            using (StreamWriter sw = new StreamWriter(keyFilePath))
+            {
+                foreach (var character in characterCodeDictionary)
+                {
+                    sw.WriteLine(character.Key + " " + character.Value);
+                }
+            }
+
+        }
+
+
+        public void CompressData()
+        {
+
+            Dictionary<char, string> characterCodeDictionary = huffmanTree.GetCharacterCodeDictionary();
+
+            string encodedData = "";
+            if (this.allCharactersFromInputFile != null)
+            {
+
+                foreach (char c in allCharactersFromInputFile)
+                {
+                    encodedData += characterCodeDictionary[c];
+                }
+
+            }
+
+            if (encodedData.Length < 8)
+            {
+                int padding = 8 - encodedData.Length;
+                encodedData = encodedData.PadLeft(encodedData.Length + padding, '0');
+            }
+
+            if (encodedData.Length % 8 != 0)
+            {
+                int padding = 8 - encodedData.Length % 8;
+                encodedData = encodedData.PadRight(encodedData.Length + padding, '0');
+            }
+
+
+            var byteArray = Enumerable.Range(0, int.MaxValue / 8)
+                          .Select(i => i * 8)
+                          .TakeWhile(i => i < encodedData.Length)
+                          .Select(i => encodedData.Substring(i, 8)) 
+                          .Select(s => Convert.ToByte(s, 2)) 
+                          .ToArray();
+
+            string outputFilePath = inputFilePath.Replace("input.txt", "output.bin");
+            File.WriteAllBytes(outputFilePath, byteArray);
+
+        }
+    }
+}
---
+++ Messaging/Modules/Data compression module/HuffmanTree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace data_compression
+{
+    internal class HuffmanTree
+    {
+        public HuffmanTreeNode? Root { get; set; }
+        private List<HuffmanTreeNode> listOfTreeNodes;
+
+        public HuffmanTree()
+        {
+            this.listOfTreeNodes = new List<HuffmanTreeNode>();
+            this.Root = null;
+        }
+
+
+        public class HuffmanTreeNode
+        {
+            public char Symbol { get; set; }
+            public int Frequency { get; set; }
+            public HuffmanTreeNode? Left { get; set; }
+            public HuffmanTreeNode? Right { get; set; }
+
+            public HuffmanTreeNode(char symbol, int frequency)
+            {
+                Left = Right = null;
+                Symbol = symbol;
+                Frequency = frequency;
+            }
+        }
+
+        public void BuildTreeFromDictionary(Dictionary<char, int> characterFrequencyDictionary)
+        {
+            foreach (var character in characterFrequencyDictionary)
+            {
+                listOfTreeNodes.Add(new HuffmanTreeNode(character.Key, character.Value));
+            }
+            BuildTree();
+        }   
+
+        public void BuildTree()
+        {
+            while (listOfTreeNodes.Count > 1)
+            {
+               
+                listOfTreeNodes.Sort((fistNode, secondNode) => fistNode.Frequency.CompareTo(secondNode.Frequency));
+
+                var leftChild = listOfTreeNodes[0];
+                var rightChild = listOfTreeNodes[1];
+
+                var parent = new HuffmanTreeNode('\0', leftChild.Frequency + rightChild.Frequency);
+                parent.Left = leftChild;
+                parent.Right = rightChild;
+
+                listOfTreeNodes.RemoveAt(0);
+                listOfTreeNodes.RemoveAt(0);
+                listOfTreeNodes.Add(parent);
+            }
+
+            this.Root = listOfTreeNodes[0];
+            
+        }
+
+
+        public Dictionary<char, string> GetCharacterCodeDictionary()
+        {
+            Dictionary<char, string> characterCodeDictionary = new Dictionary<char, string>();
+            TraverseTreeAndBuildCode(this.Root, "", characterCodeDictionary);
+            return characterCodeDictionary;
+        }
+
+        private void TraverseTreeAndBuildCode(HuffmanTreeNode? currentNode,string code, Dictionary<char, string> characterCodeDictionary)
+        {
+
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            if (currentNode.Left == null && currentNode.Right == null)
+            {
+                characterCodeDictionary.Add(currentNode.Symbol, code);
+
+            }
+            else
+            {
+                TraverseTreeAndBuildCode(currentNode.Left, code + "0", characterCodeDictionary);
+                TraverseTreeAndBuildCode(currentNode.Right, code + "1", characterCodeDictionary);
+            }
+        }
+
+    }
+}
